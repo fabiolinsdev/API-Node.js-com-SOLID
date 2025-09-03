f64a5a8b41e48eb79bec3eae20bb51310fc513e5
Fabio Lins <binhobsb_fabio29@outlook.com>
2025-09-03 17:35:24 -0300

    configurando prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+      name: 'Fabio Lins',
+      email: 'fabioLins@gamil.com'
+    },
+})
