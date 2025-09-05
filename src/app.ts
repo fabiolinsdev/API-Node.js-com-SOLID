2e4bf6e1aa7bce3bef08ef678312c73d39fc1360
Fabio Lins <binhobsb_fabio29@outlook.com>
2025-09-05 12:29:33 -0300

    Docker

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
 
 const prisma = new PrismaClient()
 
 prisma.user.create({
-    data: {
-      name: 'Fabio Lins',
-      email: 'fabioLins@gamil.com'
-    },
+  data: {
+    name: 'Fabio Lins',
+    email: 'fabioLins@gamil.com',
+  },
 })
